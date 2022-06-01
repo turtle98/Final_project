b9220afaeeabe2f566eb445fc6d02cb4927fbb3e
turtle98 <95213688+turtle98@users.noreply.github.com>
2022-06-01 15:42:00 +0900

    Update our_detr.py

--- our_detr.py
+++ our_detr.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
-
+from positional_encodings import PositionalEncoding1D
+import numpy as np
 from our_matcher import build_matcher
 #from .segmentation import (DETRsegm, PostProcessPanoptic, PostProcessSegm,
 #                           dice_loss, sigmoid_focal_loss)
@@ -63,7 +64,6 @@ class DETR(nn.Module):
             기본적으로 aspect
         """
         hs = self.transformer(input_ids, attention_masks, self.query_embed.weight, self.Positional_Encoding)
-        #hs = (498,32,768)모양의 텐서 
 
         outputs_target = self.target_embed(hs)
         #outputs_target = ()
@@ -72,3 +72,166 @@ class DETR(nn.Module):
         out = {'pred_target': outputs_target[-1], 'pred_aspect': outputs_aspect[-1], 'pred_sentiment': outputs_sentiment[-1]}
         return out
 
+class SetCriterion(nn.Module):
+    """ This class computes the loss for DETR.
+    The process happens in two steps:
+        1) we compute hungarian assignment between ground truth sand the outputs of the model
+        2) we supervise each pair of matched ground-truth / prediction (supervise class and box)
+    """
+    def __init__(self, num_targets, num_aspects, matcher, weight_dict, eos_coef1, eos_coef2, losses):
+        """ Create the criterion.
+        Parameters:
+            num_targets: number of target categories, omitting the special no-object category
+            num_aspects: number of aspect categories
+            matcher: module able to compute a matching between targets and proposals
+            weight_dict: dict containing as key the names of the losses and as values their relative weight.
+            eos_coef: relative classification weight applied to the no-object category
+            losses: list of all the losses to be applied. See get_loss for list of available losses.
+        """
+        super().__init__()
+        self.num_targets = num_targets
+        self.num_aspects = num_aspects
+        self.matcher = matcher
+        self.weight_dict = weight_dict
+        self.eos_coef1 = eos_coef1
+        self.eos_coef2 = eos_coef2
+        self.losses = losses
+        empty_weight = torch.ones(self.num_targets + 1)
+        empty_weight[-1] = self.eos_coef1
+        self.register_buffer('empty_weight', empty_weight)
+
+    def loss_targets(self, outputs, targets, indices, log=True):
+        """Classification loss (NLL)
+        targets dicts must contain the key "targets" containing a tensor of dim [nb_targets]
+        """
+        assert 'pred_target' in outputs
+        src_logits = outputs['pred_target']
+
+        idx = self._get_src_permutation_idx(indices)
+        target_classes_o = torch.cat([t["target"][J] for t, (_, J) in zip(targets, indices)])
+        target_classes = torch.full(src_logits.shape[:2], self.num_targets,
+                                    dtype=torch.int64, device=src_logits.device)
+        target_classes[idx] = target_classes_o
+
+        loss_target = F.cross_entropy(src_logits.transpose(1, 2), target_classes)
+        losses = {'loss_target': loss_target}
+
+        #if log:
+            # TODO this should probably be a separate loss, not hacked in this one here
+        #    losses['class_error'] = 100 - accuracy(src_logits[idx], target_classes_o)[0]
+        return losses
+        
+    def loss_aspects(self, outputs, targets, indices, log=True):
+        """Classification loss (NLL)
+        targets dicts must contain the key "aspects" containing a tensor of dim [nb_targets]
+        """
+        assert 'pred_aspect' in outputs
+        src_logits = outputs['pred_aspect']
+
+        idx = self._get_src_permutation_idx(indices)
+        target_aspect_o = torch.cat([t["aspect"][J] for t, (_, J) in zip(targets, indices)])
+        target_aspect = torch.full(src_logits.shape[:2], self.num_aspects,
+                                    dtype=torch.int64, device=src_logits.device)
+        target_aspect[idx] = target_aspect_o
+
+        loss_aspect = F.cross_entropy(src_logits.transpose(1, 2), target_aspect)
+        losses = {'loss_aspect': loss_aspect}
+
+        #if log:
+            # TODO this should probably be a separate loss, not hacked in this one here
+        #    losses['class_error'] = 100 - accuracy(src_logits[idx], target_classes_o)[0]
+        return losses
+
+    def loss_sentiment(self, outputs, targets, indices):
+
+        assert 'pred_sentiment' in outputs
+        idx = self._get_src_permutation_idx(indices)
+        src_sentiment = outputs['pred_sentiment'][idx]
+        target_sentiment = torch.cat([t['sentiment'][i] for t, (_, i) in zip(targets, indices)], dim=0)
+        loss_sentiment = F.mse_loss(src_sentiment, target_sentiment, reduction='mean')
+        loss_sentiment = 10*loss_sentiment
+        losses = {'loss_sentiment': loss_sentiment}
+
+        return losses
+
+    def _get_src_permutation_idx(self, indices):
+        # permute predictions following indices
+        batch_idx = torch.cat([torch.full_like(src, i) for i, (src, _) in enumerate(indices)])
+        src_idx = torch.cat([src for (src, _) in indices])
+        return batch_idx, src_idx
+
+    def _get_tgt_permutation_idx(self, indices):
+        # permute targets following indices
+        batch_idx = torch.cat([torch.full_like(tgt, i) for i, (_, tgt) in enumerate(indices)])
+        tgt_idx = torch.cat([tgt for (_, tgt) in indices])
+        return batch_idx, tgt_idx
+
+    def get_loss(self, loss, outputs, targets, indices, **kwargs):
+        loss_map = {
+            'target': self.loss_targets,
+            'aspect': self.loss_aspects,
+            'sentiment': self.loss_sentiment,
+        }
+        assert loss in loss_map, f'do you really want to compute {loss} loss?'
+        return loss_map[loss](outputs, targets, indices, **kwargs)
+
+    def forward(self, outputs, targets):
+        """ This performs the loss computation.
+        Parameters:
+             outputs: dict of tensors, see the output specification of the model for the format
+             targets: list of dicts, such that len(targets) == batch_size.
+                      The expected keys in each dict depends on the losses applied, see each loss' doc
+        """
+        outputs_without_aux = {k: v for k, v in outputs.items() if k != 'aux_outputs'}
+
+        # Retrieve the matching between the outputs of the last layer and the targets
+        indices = self.matcher(outputs_without_aux, targets)
+        
+        # Compute all the requested losses
+        losses = {}
+        for loss in self.losses:
+            losses.update(self.get_loss(loss, outputs, targets, indices))
+
+        # In case of auxiliary losses, we repeat this process with the output of each intermediate layer.
+        if 'aux_outputs' in outputs:
+            for i, aux_outputs in enumerate(outputs['aux_outputs']):
+                indices = self.matcher(aux_outputs, targets)
+                for loss in self.losses:
+                    l_dict = self.get_loss(loss, aux_outputs, targets, indices, **kwargs)
+                    l_dict = {k + f'_{i}': v for k, v in l_dict.items()}
+                    losses.update(l_dict)
+
+        return losses
+
+def build(args):
+    # the `num_classes` naming here is somewhat misleading.
+    # it indeed corresponds to `max_obj_id + 1`, where max_obj_id
+    # is the maximum id for a class in your dataset. For example,
+    # COCO has a max_obj_id of 90, so we pass `num_classes` to be 91.
+    # As another example, for a dataset that has a single class with id 1,
+    # you should pass `num_classes` to be 2 (max_obj_id + 1).
+    # For more details on this, check the following discussion
+    # https://github.com/facebookresearch/detr/issues/108#issuecomment-650269223
+
+    n = np.zeros(shape=(436, 32, 768))
+    x = torch.tensor(n, dtype=torch.float32)
+    p_enc_1d_model = PositionalEncoding1D(768)
+    p_enc = p_enc_1d_model(x)
+
+    transformer = build_transformer(1)
+    matcher = build_matcher(1)
+
+    model = DETR(
+        transformer,
+        Positional_Encoding = p_enc,
+        num_aspect = 4,
+        num_target = 226,
+        num_queries= 32,
+        aux_loss=False,
+    )
+    
+    weight_dict = {'loss_target': 1, 'loss_aspect': 1, 'loss_sentiment' : 1}
+    losses = ['target', 'aspect', 'sentiment']
+    criterion = SetCriterion(4, 226, matcher, weight_dict, 0.1 , 0.1, losses)
+
+    return criterion
